30d85ba4d2d0282bd467257324d591698eee07ea
=PuteraBhagas <=p.bhagaswara@gmail.com>
2025-03-24 22:04:55 +0700

    praktikum3

--- Week5FIX/PWL_POS/app/Models/LevelModel.php
+++ Week5FIX/PWL_POS/app/Models/LevelModel.php
@@ -4,11 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class LevelModel extends Model
 {
     use HasFactory;
 
-    protected $table = 'm_level';
-    protected $fillable = [];
+    protected $table = 'm_level'; // Nama tabel sesuai database
+    protected $primaryKey = 'level_id'; // Sesuaikan dengan primary key di database
+    public $timestamps = false; // Nonaktifkan timestamps karena tidak digunakan
+
+    protected $fillable = ['level_kode', 'level_name'];
+
+    /**
+     * Relasi ke UserModel (satu level memiliki banyak user).
+     */
+    public function users(): HasMany
+    {
+        return $this->hasMany(UserModel::class, 'level_id', 'level_id');
+    }
 }
