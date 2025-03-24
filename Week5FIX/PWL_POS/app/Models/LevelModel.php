3fcbb712c73a8d02209a30e72b7556ced16e050e
=PuteraBhagas <=p.bhagaswara@gmail.com>
2025-03-24 22:16:20 +0700

    tabel m_level

--- Week5FIX/PWL_POS/app/Models/LevelModel.php
+++ Week5FIX/PWL_POS/app/Models/LevelModel.php
@@ -1,24 +1,19 @@
 <?php
 
 namespace App\Models;
-
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+ 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+
+
 class LevelModel extends Model
 {
-    use HasFactory;
-
-    protected $table = 'm_level'; // Nama tabel sesuai database
-    protected $primaryKey = 'level_id'; // Sesuaikan dengan primary key di database
+      protected $table = 'm_level'; // Nama tabel sesuai database
+    protected $primaryKey = 'level_id'; // Pastikan primary key sesuai dengan database
+    protected $fillable = ['level_kode', 'level_name']; // Tambahkan level_kode agar bisa diakses
     public $timestamps = false; // Nonaktifkan timestamps karena tidak digunakan
-
-    protected $fillable = ['level_kode', 'level_name'];
-
-    /**
-     * Relasi ke UserModel (satu level memiliki banyak user).
-     */
+ 
     public function users(): HasMany
     {
         return $this->hasMany(UserModel::class, 'level_id', 'level_id');
