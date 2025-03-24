7a59de274672096094a9305e749ab4c0755faaeb
=PuteraBhagas <=p.bhagaswara@gmail.com>
2025-03-24 20:24:50 +0700

    praktikum 2

--- Week5FIX/PWL_POS/routes/web.php
+++ Week5FIX/PWL_POS/routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\UserController;
-
+use App\Http\Controllers\WelcomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -29,4 +29,5 @@ Route::get('/user/tambah',[UserController::class,'tambah']);
 Route::get('/user/tambah_simpan',[UserController::class,'tambah_simpan']);
 Route::get('/user/ubah/{id}',[UserController::class,'Ubah']);
 Route::get('/user/ubah_simpan/{id}',[UserController::class,'ubah_simpan']);
-Route::get('/user/hapus/{id}',[UserController::class,'hapus']);
+
+Route::get('/',[WelcomeController::class,'index']);
