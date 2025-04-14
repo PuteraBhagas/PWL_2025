080a765a01f69697742550d59954d5cc921fb399
=PuteraBhagas <=p.bhagaswara@gmail.com>
2025-04-14 15:29:03 +0700

    Implementasi final JS 4

--- POS_UTSFIX/PWL_POS/routes/web.php
+++ POS_UTSFIX/PWL_POS/routes/web.php
@@ -1,11 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
+use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\UserController;
-
 
 /*
 |--------------------------------------------------------------------------
@@ -22,6 +20,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/level',[LevelController::class,'index']);
-Route::get('/kategori',[KategoriController::class,'index']);
-Route::get('/user',[UserController::class,'index']);
+Route::get('/', function(){
+    return view('welcome');
+});
+
+Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
+Route::get('/user', [UserController::class, 'index']);
+
+Route::get('/user/tambah', [UserController::class, 'tambah']);
+Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
+
+Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
+Route::post('/user/ubah_simpan', [UserController::class, 'ubah_simpan']);
+Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
+
+Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
