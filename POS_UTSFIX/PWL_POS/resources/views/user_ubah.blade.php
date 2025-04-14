d348f3a58621904a6e877350386e84efced3cd74
=PuteraBhagas <=p.bhagaswara@gmail.com>
2025-04-14 19:04:30 +0700

    Update Implementasi js 5

--- POS_UTSFIX/PWL_POS/resources/views/user_ubah.blade.php
+++ POS_UTSFIX/PWL_POS/resources/views/user_ubah.blade.php
@@ -1,28 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Form</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
 </head>
 <body>
     <h1>Form Ubah Data User</h1>
-    <a href="/PEMROGRAMAN-WEB-LANJUT-2025/Minggu%204/PWL_POS/public/user">Kembali</a>
-    <form method="post" action="/PEMROGRAMAN-WEB-LANJUT-2025/Minggu%204/PWL_POS/public/user/ubah_simpan/{{$data->user_id}}">
-        {{ csrf_field() }}
-        {{ method_field('PUT') }}
+<a href="/user">Kembali</a>
+<br><br>
 
-        <label>Username</label>
-        <input type="text" name="username" placeholder="Masukkan Username" value="{{ $data->username }}">
-        <br>
-        <label>Nama</label>
-        <input type="text" name="nama" placeholder="Masukkan Nama" value="{{ $data->nama }}">
-        <br>
-        <label>Password</label>
-        <input type="password" name="password" placeholder="Masukkan Password" value="{{ $data->password }}">
-        <br>
-        <label>Level ID</label>
-        <input type="number" name="level_id" placeholder="Masukkan ID Level" value="{{ $data->level_id }}">
-        <br><br>
-        <input type="submit" class="btn btn-success" value="Ubah"> 
-    </form>
+<form method="post" action="/user/ubah_simpan/{{ $data->user_id }}">
+
+    {{ csrf_field() }}
+    {{ method_field('PUT') }}
+
+    <label>Username</label>
+    <input type="text" name="username" placeholder="Masukan Username" value="{{ $data->username }}">
+    <br>
+
+    <label>Nama</label>
+    <input type="text" name="nama" placeholder="Masukan Nama" value="{{ $data->username }}">
+    <br>
+
+    <label>Password</label>
+    <input type="password" name="password" placeholder="Masukan Password" value="{{ $data->password }}">
+    <br>
+
+    <label>Level ID</label>
+    <input type="number" name="level_id" placeholder="Masukan ID Level" value="{{ $data->level_id }}">
+    <br><br>
+
+    <input type="submit" class="btn btn-success" value="Ubah">
+
+</form>
 </body>
 </html>
