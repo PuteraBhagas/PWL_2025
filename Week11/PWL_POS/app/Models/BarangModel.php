5364e162876b6ea77a4e33b24c1fd5ff0f7bec02
=PuteraBhagas <=p.bhagaswara@gmail.com>
2025-04-28 20:12:12 +0700

    Tugas M_BARANG

--- Week11/PWL_POS/app/Models/BarangModel.php
+++ Week11/PWL_POS/app/Models/BarangModel.php
@@ -4,28 +4,36 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class BarangModel extends Model
 {
     use HasFactory;
 
-    protected $table = 'm_barang'; // Nama tabel di database
-    protected $primaryKey = 'barang_id'; // Primary Key
-    public $timestamps = true; // Menggunakan timestamps
-
+    protected $table = "m_barang";
+    protected $primaryKey = "barang_id";
     protected $fillable = [
+        'barang_id',
+        'kategori_id',
         'barang_kode',
         'barang_nama',
-        'kategori_id',
-        'harga_beli',
-        'harga_jual'
+        'harga_beli', 
+        'harga_jual',
+        'image',
     ];
 
-    // Relasi ke tabel kategori (m_kategori)
-    public function kategori()
+    public function image():Attribute{
+        return Attribute::make(
+            get: fn ($image) => url('/storage/post/'. $image),
+        );
+    }
+    public function kategori(): BelongsTo
     {
         return $this->belongsTo(KategoriModel::class, 'kategori_id', 'kategori_id');
     }
+    public function stok()
+    {
+        return $this->hasOne(StokModel::class, 'barang_id', 'barang_id');
+    }
 }
-    
